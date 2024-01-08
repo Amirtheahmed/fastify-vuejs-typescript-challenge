7f6b9c22a736ff492fdb319ef3a706c79644c8f8
amirahmed <amirtheahmed@gmail.com>
2024-01-08 20:47:04 +0300

    bug fix

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
-  withCredentials: false,
+  withCredentials: true,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
