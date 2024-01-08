9209c16958de398d97bcaf5f8e3105225d427c5b
amirahmed <amirtheahmed@gmail.com>
2024-01-08 19:18:13 +0300

    bug fixes

--- backend/prisma/migrations/20240104221756_added/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Category` MODIFY `parentId` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `Category` ADD CONSTRAINT `Category_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
